12cf44bbe18c539566f8f339ce1b9baaf944c895
Tanvi Mandaliya <tanvi.mandaliya@tssconsultancy.com>
2026-05-19 09:30:04 +0530

    chnages in some space.

--- src/app/components/pagination/pagination.ts
+++ src/app/components/pagination/pagination.ts
@@ -1,12 +1,5 @@
 import { CommonModule } from '@angular/common';
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  Output,
-  SimpleChanges
-} from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
@@ -19,61 +12,45 @@ export class Pagination implements OnChanges {
 
   @Input() users: any[] = [];
 
-  @Output() paginatedUsers =
-    new EventEmitter<any[]>();
+  @Output() paginatedUsers = new EventEmitter<any[]>();
 
   currentPage = 1;
-
   itemsPerPage = 8;
 
-  ngOnChanges(changes: SimpleChanges): void {
+  ngOnChanges() {
     this.sendPaginatedData();
   }
 
   sendPaginatedData() {
-
-    const start =
-      (this.currentPage - 1) * this.itemsPerPage;
-
-    const end =
-      start + this.itemsPerPage;
+    const start = (this.currentPage - 1) * this.itemsPerPage;
+    const end = start + this.itemsPerPage;
 
     setTimeout(() => {
-
       this.paginatedUsers.emit(
         this.users.slice(start, end)
       );
-
     });
 
   }
 
 
   getTotalPages() {
-
     return Math.ceil(
       this.users.length / this.itemsPerPage
     );
   }
 
   nextPage() {
-
     if (this.currentPage < this.getTotalPages()) {
-
       this.currentPage++;
-
       this.sendPaginatedData();
     }
   }
 
   previousPage() {
-
     if (this.currentPage > 1) {
-
       this.currentPage--;
-
       this.sendPaginatedData();
     }
   }
-
 }
